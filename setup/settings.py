2768a045a5c5fe538f2885e6165ec58d6e1bc5f2
Marrowsed <marcus.ah@gmail.com>
2022-05-05 14:02:21 -0300

    Semana 02 - BCrypt

--- setup/settings.py
+++ setup/settings.py
@@ -104,6 +104,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+]
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
